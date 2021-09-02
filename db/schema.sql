1eb9b85e6deb1d016abb5cba139aeb6c34eb2db4
Justin Phillips <Jukphillips1990@gmail.com>
2021-09-01 21:23:09 -0600

    Developed schema to include all requirements and began building seed file

--- db/schema.sql
+++ db/schema.sql
@@ -10,11 +10,14 @@ CREATE TABLE department (
 );
 
 DROP TABLE IF EXISTS role;
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT 
+    department_id INT NOT NULL ,
+    FOREIGN KEY(department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
 );
 
 DROP TABLE IF EXISTS employee;
@@ -22,6 +25,9 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
+    role_id INT NOT NULL,
+    FOREIGN KEY(role_id) 
+    REFERENCES department(id)
+    ON DELETE SET NULL,
     manager_id INT 
-)
+);
